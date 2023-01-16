37f5d52da2019daf81609ae6e7fd32e530db9973
Tcharles W. Barreto <tcharlesigt@gmail.com>
2023-01-16 14:13:41 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest } from '../../presentation/protocols'
+import { Request, RequestHandler, Response } from 'express'
+
+export const adaptRoute = (controller: Controller): RequestHandler => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
